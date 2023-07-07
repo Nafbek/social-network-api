592f77de199e0c54e1269fa2411278f98f4a6bf6
Bekam <mspbekan@gmail.com>
2023-07-07 16:52:50 -0500

    set up  the complete User model

--- models/User.js
+++ models/User.js
@@ -3,8 +3,49 @@ const { Schema, Types } = require('mongoose');
 
 // Schema to create user model
 const userSchema = new Schema(
- 
+
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+            validate: {
+                validator: (v)=> {
+                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v)
+                    // '$jsonSchema': { pattern: '@gmail.com' },
+                    // $match: '@gmail.com'
+
+                }
+            },
+        },
+        thoughts: [{
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }],
+        friends: [{
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }],
+
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false,
+    }
 );
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
 
 const User = model('user', userSchema)
 
