b8fce588986a91ea75b3304dda7a1966f9ba6680
Bekam <mspbekan@gmail.com>
2023-07-08 17:50:39 -0500

    controllers and API endpoints for User route developed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,68 @@
-const { ObjectId } = require('mongoose').Types;
-const { User, Thought } = require('../models');
+const { ObjectId } = require("mongoose").Types;
+const { User, Thought } = require("../models");
 
 module.exports = {
-  
+  //Get all users
+  async getUsers(req, res) {
+    try {
+      const result = await User.find();
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  // Get a single user
+  async getSingleUser(req, res) {
+    try {
+      const result = await User.findOne({ _id: req.params.userId }).populate(
+        "thought",
+        "user"
+      ); //????????
+      if (!result) {
+        return res.status(404).json({ message: "No user with that Id" });
+      }
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async createUser(req, res) {
+    try {
+      const result = await User.create(req.body);
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async updateUser(req, res) {
+    try {
+      const result = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+      if (!result) {
+        return res.status(404).json({ message: "No user with that Id" });
+      }
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteUser(req, res) {
+    try {
+      const result = await User.findOneAndDelete({ _id: req.params.userId });
+      await Thought.deleteMany({ user: req.params.userId });
+      if (!result) {
+        return res.status(404).json({ message: "No user with that Id" });
+      }
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 };
--- models/User.js
+++ models/User.js
@@ -1,53 +1,53 @@
-const { Schema, Types } = require('mongoose');
-const Thought = require('./Thought')
-
+const { Schema, Types } = require("mongoose");
+const Thought = require("./Thought");
 
 // Schema to create user model
 const userSchema = new Schema(
-
-    {
-        username: {
-            type: String,
-            required: true,
-            trim: true,
-            unique: true,
-        },
-        email: {
-            type: String,
-            required: true,
-            trim: true,
-            unique: true,
-            validate: {
-                validator: (v)=> {
-                    return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v)
-                    // '$jsonSchema': { pattern: '@gmail.com' },
-                    // $match: '@gmail.com'
-
-                }
-            },
-        },
-        thoughts: [{
-            type: Schema.Types.ObjectId,
-            ref: 'Thought'
-        }],
-        friends: [{
-            type: Schema.Types.ObjectId,
-            ref: 'User'
-        }],
-
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
-    {
-        toJSON: {
-            virtuals: true
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      validate: {
+        validator: (v) => {
+          return /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/.test(v);
+          // '$jsonSchema': { pattern: '@gmail.com' },
+          // $match: '@gmail.com'
         },
-        id: false,
-    }
+      },
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
 );
 
-userSchema.virtual('friendCount').get(function () {
-    return this.friends.length;
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
 });
 
-const User = model('user', userSchema)
+const User = model("user", userSchema);
 
 module.exports = User;
