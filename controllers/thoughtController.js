b63727f0d3f21373c55bcc2b85391b1af74ddf94
Bekam <mspbekan@gmail.com>
2023-07-09 21:18:40 -0500

    some api endpoints for the thought route developed

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,3 +1,65 @@
+const { ObjectId } = require("mongoose").Types;
+const { User, Thought } = require("../models");
 
-const { ObjectId } = require('mongoose').Types;
-const { User, Thought } = require('../models');
+module.exports = {
+  async getThoughts(req, res) {
+    try {
+      const result = await Thought.find();
+      if (!result) {
+        res.status(400).json({ message: "Thought not found" });
+      }
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async getSingleThought(req, res) {
+    try {
+      const result = await Thought.findOne({ _id: req.params.thoughtId });
+
+      if (!result) {
+        res.status(400).json({ message: "Thought with that Id not found" });
+      }
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async createThought(req, res) {
+    const result = await Thought.create(req.body);
+  },
+
+  async updateThought(req, res) {
+    try {
+      const result = await Thought.findOneAndUpdate(
+        { _id: req.thoughtId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!result) {
+        res.status(400).json({ message: "Thought with that Id not found" });
+      }
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+
+  async deleteThought(req, res) {
+    try {
+      const result = await Thought.findOneAndDelete({
+        _id: req.params.thoughtId,
+      });
+
+      if (!result) {
+        res.status(400).json({ message: "Thought with that Id not found" });
+      }
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
